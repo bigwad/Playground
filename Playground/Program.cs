343c62ca899400c92a08fdd3666c05a5b2a31b77
Konstantin <const.miyang@gmail.com>
2016-09-09 19:45:51 +0200

    Format code

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,17 +6,23 @@ using System.Collections.Generic;
 using Starcounter;
 using Playground.Database;
 
-namespace Playground {
-    class Program {
-        static void Main() {
+namespace Playground
+{
+    class Program
+    {
+        static void Main()
+        {
             Application.Current.Use(new HtmlFromJsonProvider());
             Application.Current.Use(new CustomPartialToStandaloneHtmlProvider());
 
             RegisterMasterChildren();
 
-            Hook<Item>.BeforeDelete += (sender, entity) => {
-                Session.ForAll((s, id) => {
-                    if (s == null || !(s.Data is IndexPage)) {
+            Hook<Item>.BeforeDelete += (sender, entity) =>
+            {
+                Session.ForAll((s, id) =>
+                {
+                    if (s == null || !(s.Data is IndexPage))
+                    {
                         return;
                     }
 
@@ -27,9 +33,12 @@ namespace Playground {
                 });
             };
 
-            Hook<Item>.CommitDelete += (sender, entity) => {
-                Session.ForAll((s, id) => {
-                    if (s == null || !(s.Data is IndexPage)) {
+            Hook<Item>.CommitDelete += (sender, entity) =>
+            {
+                Session.ForAll((s, id) =>
+                {
+                    if (s == null || !(s.Data is IndexPage))
+                    {
                         return;
                     }
 
@@ -40,9 +49,12 @@ namespace Playground {
                 });
             };
 
-            Hook<Item>.CommitInsert += (sender, entity) => {
-                Session.ForAll((s, id) => {
-                    if (s == null || !(s.Data is IndexPage)) {
+            Hook<Item>.CommitInsert += (sender, entity) =>
+            {
+                Session.ForAll((s, id) =>
+                {
+                    if (s == null || !(s.Data is IndexPage))
+                    {
                         return;
                     }
 
@@ -54,9 +66,12 @@ namespace Playground {
                 });
             };
 
-            Hook<Item>.CommitUpdate += (sender, entity) => {
-                Session.ForAll((s, id) => {
-                    if (s == null || !(s.Data is IndexPage)) {
+            Hook<Item>.CommitUpdate += (sender, entity) =>
+            {
+                Session.ForAll((s, id) =>
+                {
+                    if (s == null || !(s.Data is IndexPage))
+                    {
                         return;
                     }
 
@@ -68,20 +83,27 @@ namespace Playground {
                 });
             };
 
-            Handle.GET("/playground", () => {
-                if (Session.Current == null) {
+            Handle.GET("/playground", () =>
+            {
+                if (Session.Current == null)
+                {
                     Session.Current = new Session(SessionOptions.PatchVersioning);
                 }
 
-                return Db.Scope<IndexPage>(() => {
+                return Db.Scope<IndexPage>(() =>
+                {
                     IndexPage page = Session.Current.Data as IndexPage;
 
-                    if (page == null) {
-                        page = new IndexPage() {
+                    if (page == null)
+                    {
+                        page = new IndexPage()
+                        {
                             Session = Session.Current,
                             Data = null
                         };
-                    } else {
+                    }
+                    else
+                    {
                         page.Data = null;
                     }
 
@@ -89,35 +111,45 @@ namespace Playground {
                 });
             });
 
-            Handle.GET("/playground/reset", () => {
-                if (Session.Current != null && Session.Current.Data is IndexPage) {
+            Handle.GET("/playground/reset", () =>
+            {
+                if (Session.Current != null && Session.Current.Data is IndexPage)
+                {
                     IndexPage page = Session.Current.Data as IndexPage;
 
                     page.Data = null;
 
                     return page;
-                } else {
+                }
+                else
+                {
                     return Self.GET("/playground");
                 }
             });
 
-            Handle.GET("/playground/change", () => {
-                if (Session.Current != null && Session.Current.Data is IndexPage) {
+            Handle.GET("/playground/change", () =>
+            {
+                if (Session.Current != null && Session.Current.Data is IndexPage)
+                {
                     IndexPage page = Session.Current.Data as IndexPage;
 
                     page.MessageText = "/playground/change was opened";
 
                     return page;
-                } else {
+                }
+                else
+                {
                     return Self.GET("/playground");
                 }
             });
 
-            Handle.GET("/playground/sub", () => new SubPage() {
+            Handle.GET("/playground/sub", () => new SubPage()
+            {
                 Data = null
             });
 
-            Handle.GET("/playground/date", () => new DatePage() {
+            Handle.GET("/playground/date", () => new DatePage()
+            {
                 Data = null
             });
 
@@ -125,21 +157,25 @@ namespace Playground {
 
             Handle.GET("/playground/matrix", () => new MatrixPage() { Data = null });
 
-            Handle.GET("/playground/details/{?}", (string id) => new SubDetailsPage() {
+            Handle.GET("/playground/details/{?}", (string id) => new SubDetailsPage()
+            {
                 Data = id
             });
 
-            Handle.GET("/playground/ractive", () => new RactivePage() {
+            Handle.GET("/playground/ractive", () => new RactivePage()
+            {
                 Session = new Session(SessionOptions.PatchVersioning),
                 Data = null
             });
 
-            Handle.GET("/playground/ractive/{?}", (string value) => new RactiveSubPage() {
+            Handle.GET("/playground/ractive/{?}", (string value) => new RactiveSubPage()
+            {
                 Name = value.Split(".".ToCharArray())[0],
                 Price = decimal.Parse(value.Split(".".ToCharArray())[1])
             });
 
-            Handle.GET("/playground/insert", () => {
+            Handle.GET("/playground/insert", () =>
+            {
                 Data data = new Data();
 
                 data.Insert();
@@ -147,7 +183,8 @@ namespace Playground {
                 return "Data inserted";
             });
 
-            Handle.GET("/playground/delete", () => {
+            Handle.GET("/playground/delete", () =>
+            {
                 Data data = new Data();
 
                 data.Delete();
@@ -155,18 +192,21 @@ namespace Playground {
                 return "Data deleted";
             });
 
-            Handle.GET("/playground/parse-query?{?}", (Request r, string s) => {
+            Handle.GET("/playground/parse-query?{?}", (Request r, string s) =>
+            {
                 var query = System.Web.HttpUtility.ParseQueryString(s);
                 StringBuilder sb = new StringBuilder();
 
-                foreach (string item in query.Keys) {
+                foreach (string item in query.Keys)
+                {
                     sb.Append(item).Append(": ").Append(query[item]).Append(Environment.NewLine);
                 }
 
                 return sb.ToString();
             }, new HandlerOptions() { SkipRequestFilters = true });
 
-            Handle.GET("/playground/benchmark/{?}", (int times) => {
+            Handle.GET("/playground/benchmark/{?}", (int times) =>
+            {
                 StringBuilder html = new StringBuilder();
                 Stopwatch watch = new Stopwatch();
 
@@ -185,7 +225,8 @@ namespace Playground {
 
                 watch.Reset();
                 watch.Start();
-                for (int i = 0; i < times; i++) {
+                for (int i = 0; i < times; i++)
+                {
                     var motherboards = Db.SQL<Motherboard>("SELECT c.Motherboard FROM Playground.MotherboardCpu c WHERE c.Cpu.Name LIKE ? GROUP BY c.Motherboard", "%i7-5%").ToList();
                 }
                 watch.Stop();
@@ -193,7 +234,8 @@ namespace Playground {
 
                 watch.Reset();
                 watch.Start();
-                for (int i = 0; i < times; i++) {
+                for (int i = 0; i < times; i++)
+                {
                     var motherboards = Db.SQL<MotherboardCpu>("SELECT c FROM Playground.MotherboardCpu c WHERE c.Cpu.Name LIKE ?", "%i7-5%").Select(x => x.Motherboard).Distinct().ToList();
                 }
                 watch.Stop();
@@ -201,7 +243,8 @@ namespace Playground {
 
                 watch.Reset();
                 watch.Start();
-                for (int i = 0; i < times; i++) {
+                for (int i = 0; i < times; i++)
+                {
                     var motherboards = Db.SQL<Motherboard>("SELECT c.Motherboard FROM Playground.MotherboardCpu c WHERE c.Cpu.Name LIKE ?", "%i7-5%").Distinct().ToList();
                 }
                 watch.Stop();
@@ -218,10 +261,12 @@ namespace Playground {
                 return html.ToString();
             });
 
-            Handle.GET("/playground/populate-json", () => {
+            Handle.GET("/playground/populate-json", () =>
+            {
                 string json = "[{ \"Name\": \"fx-8350\", \"Socket\": \"AM3\" }, { \"Name\": \"i7-6850k\", \"Socket\": \"2011-3\", \"Reviews\": [{ \"Name\": \"John\", \"Score\": 10 }, { \"Name\": \"Max\", \"Score\": 9 }] }]";
 
-                Db.Transact(() => {
+                Db.Transact(() =>
+                {
                     List<Cpu> cpus = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Cpu>>(json);
                 });
 
@@ -230,14 +275,17 @@ namespace Playground {
 
             Handle.GET("/playground/dynitems", () => new DynItemsPage() { Data = null });
 
-            Handle.GET("/playground/compare-counters/{?}", (int count) => {
+            Handle.GET("/playground/compare-counters/{?}", (int count) =>
+            {
                 Stopwatch watch = new Stopwatch();
                 ulong[] array = new ulong[count];
                 StringBuilder sb = new StringBuilder();
 
                 watch.Start();
-                array.AsParallel().ForAll(i => {
-                    Scheduling.ScheduleTask(() => {
+                array.AsParallel().ForAll(i =>
+                {
+                    Scheduling.ScheduleTask(() =>
+                    {
                         Counter.GetNextValue("");
                     }, true);
                 });
@@ -246,8 +294,10 @@ namespace Playground {
 
                 watch.Reset();
                 watch.Start();
-                array.AsParallel().ForAll(i => {
-                    Scheduling.ScheduleTask(() => {
+                array.AsParallel().ForAll(i =>
+                {
+                    Scheduling.ScheduleTask(() =>
+                    {
                         LockCounter.GetNextValue();
                     }, true);
                 });
@@ -256,8 +306,10 @@ namespace Playground {
 
                 watch.Reset();
                 watch.Start();
-                array.AsParallel().ForAll(i => {
-                    Scheduling.ScheduleTask(() => {
+                array.AsParallel().ForAll(i =>
+                {
+                    Scheduling.ScheduleTask(() =>
+                    {
                         Counter.GetNextValue((i % 15).ToString());
                     }, true);
                 });
@@ -266,8 +318,10 @@ namespace Playground {
 
                 watch.Reset();
                 watch.Start();
-                array.AsParallel().ForAll(i => {
-                    Scheduling.ScheduleTask(() => {
+                array.AsParallel().ForAll(i =>
+                {
+                    Scheduling.ScheduleTask(() =>
+                    {
                         LockCounter.GetNextValue((i % 15).ToString());
                     }, true);
                 });
@@ -280,21 +334,27 @@ namespace Playground {
             Starcounter.Internal.JsonResponseMerger.RegisterMergeCallback(OnJsonMerge);
         }
 
-        static Json OnJsonMerge(Request request, string callingAppName, IEnumerable<Json> partialJsons) {
+        static Json OnJsonMerge(Request request, string callingAppName, IEnumerable<Json> partialJsons)
+        {
             return null;
         }
 
-        static void RegisterMasterChildren() {
-            Handle.GET("/Playground/Child/{?}", (string title) => {
+        static void RegisterMasterChildren()
+        {
+            Handle.GET("/Playground/Child/{?}", (string title) =>
+            {
                 MasterPage master = Session.Current?.Data as MasterPage;
 
-                if (master == null) {
-                    master = new MasterPage() {
+                if (master == null)
+                {
+                    master = new MasterPage()
+                    {
                         Session = new Session(SessionOptions.PatchVersioning)
                     };
                 }
 
-                master.CurrentPage = new ChildPage() {
+                master.CurrentPage = new ChildPage()
+                {
                     Data = DateTime.Now.ToString(),
                     Title = title,
                     SessionId = Session.Current.SessionId
