191bff77cb93750bca66e75fd8d83c8c2081d4bc
Konstantin <const.miyang@gmail.com>
2019-01-25 15:30:10 +0100

    Update palindrom.yml scenario for artillery.io

--- artillery.io/palindrom.js
+++ artillery.io/palindrom.js
@@ -1,8 +1,9 @@
 module.exports = {
     logPalindromModel: logPalindromModel,
     insertItem: insertItem,
-    setLastItemGuid: setLastItemGuid,
-    updateLastItem: updateLastItem
+    setInsertedItemGuid: setInsertedItemGuid,
+    updateInsertedItem: updateInsertedItem,
+    deleteInsertedItem: deleteInsertedItem
 };
 
 function logPalindromModel(context, events, done) {
@@ -10,39 +11,27 @@ function logPalindromModel(context, events, done) {
     return done();
 }
 
-function insertItem(context, events, done) {
-    // {"_ver#c$":0,"_ver#s":0,"Playground_0":{"Html":"/Playground/IndexPage.html","Items":[],"InsertTrigger$":0}}
-    palindromAction(function (obj) {
-        obj.Playground_0.InsertTrigger$++;
-    }, context, events, done);
+function insertItem(context, events, obj) {
+    obj.Playground_0.InsertTrigger$++;
 }
 
-function setLastItemGuid(context, events, done) {
-    // {"_ver#c$":0,"_ver#s":0,"Playground_0":{"Html":"/Playground/IndexPage.html","Items":[{"Id":"Pq","ObjectNo":1002,"Guid$":"82a19499-3b91-47e9-a7f2-2db20fed99e3","Date$":"2019-01-24 15:34:16Z","Thread$":0,"Index$":0,"Notes":null,"DeleteTrigger$":0,"UpdateTrigger$":0}],"InsertTrigger$":0}}
-    palindromAction(function (obj) {
-        const len = obj.Playground_0.Items.length;
-        const item = obj.Playground_0.Items[len - 1];
-        item.Guid$ = "Aritllery.io - " + new Date().toString();
-    }, context, events, done);
-}
-
-function updateLastItem(context, events, done) {
-    // {"_ver#c$":0,"_ver#s":0,"Playground_0":{"Html":"/Playground/IndexPage.html","Items":[{"Id":"Pq","ObjectNo":1002,"Guid$":"82a19499-3b91-47e9-a7f2-2db20fed99e3","Date$":"2019-01-24 15:34:16Z","Thread$":0,"Index$":0,"Notes":null,"DeleteTrigger$":0,"UpdateTrigger$":0}],"InsertTrigger$":0}}
-    palindromAction(function (obj) {
-        const len = obj.Playground_0.Items.length;
-        const item = obj.Playground_0.Items[len - 1];
-        item.UpdateTrigger$++;
-    }, context, events, done);
-}
-
-function palindromAction(action, context, events, done) {
-    const originalOnRemoteChange = context.palindrom.onRemoteChange;
+function setInsertedItemGuid(context, events, obj) {
+    const no = obj.Playground_0.InsertedObjectNo;
+    const item = obj.Playground_0.Items.find(x => x.ObjectNo == no);
     
-    context.palindrom.onRemoteChange = function(sequence, results) {
-        originalOnRemoteChange(sequence, results);
-        context.palindrom.onRemoteChange = originalOnRemoteChange;
-        done();
-    };
+    item.Guid$ = "Aritllery.io - " + new Date().toString();
+}
+
+function updateInsertedItem(context, events, obj) {
+    const no = obj.Playground_0.InsertedObjectNo;
+    const item = obj.Playground_0.Items.find(x => x.ObjectNo == no);
     
-    action(context.palindrom.obj);
+    item.UpdateTrigger$++;
+}
+
+function deleteInsertedItem(context, events, obj) {
+    const no = obj.Playground_0.InsertedObjectNo;
+    const item = obj.Playground_0.Items.find(x => x.ObjectNo == no);
+    
+    item.DeleteTrigger$++;
 }
