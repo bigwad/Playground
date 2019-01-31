46ff5b1ff215801bff226823566f1879b83e76d2
Konstantin <const.miyang@gmail.com>
2019-01-31 06:30:17 +0100

    Comment out Subground blending access

--- Subground/Program.cs
+++ Subground/Program.cs
@@ -15,8 +15,8 @@ namespace Subground
 
             Handle.GET("/subground/table-row-item/{?}", (string id) => new TableRowItemJson(id));
             Handle.GET("/subground/table-columns-items", () => new TableColumnsItemsJson());
-            Starcounter.Advanced.Blender.MapUri<Database.Person>("/subground/table-row-item/{?}", new string[0]);
-            Starcounter.Advanced.Blender.MapUri("/subground/table-columns-items", $"table-columns-{typeof(Database.Person)}", new string[0]);
+            //Starcounter.Advanced.Blender.MapUri<Database.Person>("/subground/table-row-item/{?}", new string[0]);
+            //Starcounter.Advanced.Blender.MapUri("/subground/table-columns-items", $"table-columns-{typeof(Database.Person)}", new string[0]);
 
             Handle.GET("/subground/items/partial/{?}", (string id) =>
             {
