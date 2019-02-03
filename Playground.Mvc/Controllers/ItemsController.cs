8314499cc2133f2edb22477da61755d988a33b30
Konstantin <const.miyang@gmail.com>
2019-02-03 21:58:19 +0100

    Implement Playground.Mvc project

---
+++ Playground.Mvc/Controllers/ItemsController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+using Playground.Mvc.Models;
+
+namespace Playground.Mvc.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        protected PlaygroundEntities db = new PlaygroundEntities();
+
+        // GET api/<controller>
+        public IEnumerable<ItemProxy> Get()
+        {
+            IEnumerable<ItemProxy> items = db.Items.OrderBy(x => x.Date).ToArray().Select(x => new ItemProxy(x));
+            return items;
+        }
+
+        // GET api/<controller>/5
+        public ItemProxy Get(int id)
+        {
+            Item item = db.Items.FirstOrDefault(x => x.Id == id);
+
+            if (item == null)
+            {
+                return new ItemProxy();
+            }
+
+            ItemProxy proxy = new ItemProxy(item);
+
+            return proxy;
+        }
+
+        // POST api/<controller>
+        public ItemProxy Post([FromBody]ItemProxy value)
+        {
+            if (value.Guid == null)
+            {
+                value.Guid = Guid.NewGuid();
+            }
+
+            value.Date = DateTime.Now;
+            value.Insert(db);
+
+            return value;
+        }
+
+        // PUT api/<controller>
+        public ItemProxy Put([FromBody]ItemProxy value)
+        {
+            value.Update(db);
+            return value;
+        }
+
+        // DELETE api/<controller>/5
+        public bool Delete(int id)
+        {
+            Item item = db.Items.FirstOrDefault(x => x.Id == id);
+
+            if (item == null)
+            {
+                return false;
+            }
+
+            ItemProxy proxy = new ItemProxy(item);
+            proxy.Delete(db);
+
+            return true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Playground.Mvc/Models/Proxy/ItemProxy.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Playground.Mvc.Models
+{
+    public class ItemProxy
+    {
+        public long Id { get; set; }
+        public Guid? Guid { get; set; }
+        public DateTime Date { get; set; }
+        public int Thread { get; set; }
+        public int Index { get; set; }
+        public string Notes { get; set; }
+
+        public string DateStr
+        {
+            get
+            {
+                return this.Date.ToString();
+            }
+            set
+            {
+                DateTime date;
+
+                if (DateTime.TryParse(value, out date))
+                {
+                    this.Date = date;
+                }
+            }
+        }
+
+        public ItemProxy()
+        {
+        }
+
+        public ItemProxy(Item source)
+        {
+            this.Id = source.Id;
+            this.Guid = source.Guid;
+            this.Date = source.Date;
+            this.Thread = source.Thread;
+            this.Index = source.Index;
+        }
+
+        public void Update(PlaygroundEntities db)
+        {
+            Item item = db.Items.FirstOrDefault(x => x.Id == this.Id);
+
+            if (item == null)
+            {
+                throw new InvalidOperationException($"Item with Id {this.Id} not found.");
+            }
+
+            item.Guid = this.Guid;
+            item.Date = this.Date;
+            item.Thread = this.Thread;
+            item.Index = this.Index;
+
+            db.SaveChanges();
+        }
+
+        public void Insert(PlaygroundEntities db)
+        {
+            Item item = new Item();
+
+            item.Guid = this.Guid;
+            item.Date = this.Date;
+            item.Thread = this.Thread;
+            item.Index = this.Index;
+
+            db.Items.Add(item);
+            db.SaveChanges();
+
+            this.Id = item.Id;
+        }
+
+        public void Delete(PlaygroundEntities db)
+        {
+            Item item = db.Items.FirstOrDefault(x => x.Id == this.Id);
+
+            if (item == null)
+            {
+                throw new InvalidOperationException($"Item with Id {this.Id} not found.");
+            }
+
+            db.Items.Remove(item);
+            db.SaveChanges();
+        }
+    }
+}
