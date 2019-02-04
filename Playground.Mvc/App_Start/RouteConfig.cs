ad3f533ef446ceda6737a7e8aa1c2b11032f7f29
Konstantin <const.miyang@gmail.com>
2019-02-04 02:19:22 +0100

    Update NuGet packages and remove ApplicationInsights

--- Playground.Mvc/App_Start/RouteConfig.cs
+++ Playground.Mvc/App_Start/RouteConfig.cs
@@ -11,14 +11,13 @@ namespace Playground.Mvc
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.MapMvcAttributeRoutes();
-
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "Playground.Mvc.Controllers" }
             );
         }
     }
