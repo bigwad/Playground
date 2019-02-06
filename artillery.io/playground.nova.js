c77f04cc8133b8ce0658763c5c7ca76582b09dba
Konstantin <const.miyang@gmail.com>
2019-02-07 00:17:23 +0100

    Add playground.nova.yml artillery.io scenario

---
+++ artillery.io/playground.nova.js
@@ -0,0 +1,46 @@
+module.exports = {
+    setInsertItemData: setInsertItemData,
+    setUpdateItemData: setUpdateItemData,
+    setRandomItemOid: setRandomItemOid,
+    setItemsLength: setItemsLength
+};
+
+function setInsertItemData(context, events, done) {
+    context.vars["InsertItem"] = {
+        "Guid": "artillery.io",
+        "Date": new Date(),
+        "Thread": 0,
+        "Index": 0,
+        "Notes": null
+    };
+
+    return done();
+}
+
+function setUpdateItemData(context, events, done) {
+    const viewItem = context.vars["ViewItem"];
+
+    viewItem.Guid = "artillery.io - " + new Date().toString();
+    context.vars["UpdateItem"] = viewItem;
+
+    return done();
+}
+
+function setRandomItemOid(context, events, done) {
+    const items = context.vars["Items"];
+    const i = Math.floor(Math.random() * items.length);
+    const randomItem = items[i];
+
+    context.vars["RandomItemOid"] = randomItem.Oid;
+
+    return done();
+}
+
+function setItemsLength(context, events, done) {
+    const items = context.vars["Items"];
+    const length = items.length;
+
+    context.vars["ItemsLength"] = length;
+
+    return done();
+}
