d5f3bc72e6de84c08fad6a391dc0b5d214392f1f
Konstantin <const.miyang@gmail.com>
2019-02-12 11:32:36 +0100

    Format Item.Date index creation

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -16,17 +16,25 @@ namespace Playground
 {
     class Program
     {
-        static public void CreateIndex(string index_name, string query)
+        static public void CreateIndex(string indexName, string query)
         {
-            bool index_exist = false;
-            Db.Transact(() => index_exist = Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", index_name).Any());
-            if (!index_exist)
+            bool indexExist = false;
+            
+            Db.Transact(() =>
+            {
+                indexExist = Db.SQL("SELECT i FROM Starcounter.Metadata.\"Index\" i WHERE Name = ?", indexName).Any();
+            });
+            
+            if (!indexExist)
+            {
                 Db.SQL(query);
+            }
         }
 
         static void Main()
         {
-            CreateIndex("Item_Date", $"CREATE INDEX Item_Date ON {nameof(Database.Item)} ({nameof(Database.Item.Date)})");
+            string name = "IX_Item_Date";
+            CreateIndex(name, $"CREATE INDEX {name} ON {nameof(Database.Item)} ({nameof(Database.Item.Date)})");
 
             //RegisterDatabaseHooks();
             RegisterRestApi();
